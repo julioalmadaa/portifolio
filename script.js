87a6faee49df0d74789130618b7dcfb8035b9c82
Julio <arch.almada@gmail.com>
2022-11-11 21:26:44 -0300

    :package: calculo de orcamento

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
 document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
-);clear
+);
+
+document.querySelector("#qtde").addEventListener("change", atualizaPreco)
+document.querySelector("#js").addEventListener("change", atualizaPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizaPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizaPreco)
+document.querySelector("#prazo").addEventListener("change", function () {
+    const prazo = document.querySelector("#prazo").value
+    document.querySelector("label[for=prazo]").innerHTML = `Prazo: ${prazo} semana`
+    atualizaPreco()
+})
+
+function atualizaPreco() {
+    const qtde = document.querySelector("#qtde").value
+    const temJS = document.querySelector("#js").checked
+    const incluilayout = document.querySelector("#layout-sim").checked
+    const prazo = document.querySelector("#prazo").value
+    let preco = qtde * 100;
+    if (temJS) preco *= 1.1
+    if (incluilayout) preco += 500
+    let taxaUrgencia = 1 - prazo*0.1
+    preco *= 1 + taxaUrgencia
+
+    document.querySelector("#preco").innerHTML = `R$ ${preco.toFixed(2)}` 
+    console.log(qtde)
+}
